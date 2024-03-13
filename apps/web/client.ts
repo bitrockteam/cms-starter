4b099231cc781dc69b7cadff68abcee593ba88b6
erikfazio <fazioerik@gmail.com>
2024-03-13 15:08:41 +0100

    fix: typos on sanity client for web app

--- apps/web/client.ts
+++ apps/web/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export default createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_PROJECT_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_PROJECT_DATASET,
   useCdn: true, // `false` if you want to ensure fresh data
 });
