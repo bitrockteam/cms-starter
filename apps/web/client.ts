305b7449ce4a16704a931158c82cb7e1685ed12e
erikfazio <fazioerik@gmail.com>
2024-03-15 11:59:58 +0100

    build: add env variable for sanity api version

--- apps/web/client.ts
+++ apps/web/client.ts
@@ -3,6 +3,6 @@ import { createClient } from "@sanity/client";
 export default createClient({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: process.env.SANITY_STUDIO_PROJECT_DATASET,
+  apiVersion: process.env.SANITY_STUDIO_API_VERSION,
   useCdn: true, // `false` if you want to ensure fresh data,
-  apiVersion: "2024-01-30",
 });
